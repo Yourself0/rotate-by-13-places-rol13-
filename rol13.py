aadf23cff1b4a8675330fe4a74761c1d50672d9d
Yourself0 <64607990+Yourself0@users.noreply.github.com>
2022-12-22 13:19:05 +0530

    Add files via upload

--- rol13.py
+++ rol13.py
@@ -1,33 +1,32 @@
-import string
+import string # string to all alphabet (a-z)
 
 def string_roll_for_13(strings,roll_forward):
-    alpha = list(string.ascii_lowercase)
-    number_saver=0
+    alpha = list(string.ascii_lowercase) # getting a-z in list as alpha 
+    number_saver=0      
     new_string_out=''
     wrd_list=[]
-    #roll_forward = 1
     for words in strings:
     	if words.isalpha():
-    		if words in alpha:
-    			for v,k in enumerate(alpha):
-    				if words in k:
+    		if words in alpha:               # checking input words match with list in alpha
+    			for v,k in enumerate(alpha): # seperating list key and value
+    				if words in k:           # same as 10 line 
     					v +=roll_forward
-    					if v >= len(alpha)-1:
+    					if v >= len(alpha)-1: # if key is greater than index of alpha 
     						number_saver = abs(v-(len(alpha)))
     						wrd_list.append(alpha[number_saver])
     					else:
     							wrd_list.append(alpha[v])
     	
     	
-    	else:
+    	else:     # if input contains number or symbols are skipped and added here in output
     			wrd_list.append(words)
-    return wrd_list
+    return wrd_list # final word list
     
 if __name__=='__main__':
-    roll = 13
+    roll = 13 # to rotate by 13 places in alphabet
     print("ROT13 \n ")
-    strings = input("Enter string : ")
-    string_out=string_roll_for_13(strings.lower(),roll)
+    strings = input("Enter string : ") # input string 
+    string_out  =  string_roll_for_13(strings.lower(),roll) # calling function 
     new_string = ""
     for i in string_out:
     	new_string +=i
